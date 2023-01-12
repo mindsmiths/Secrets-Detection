943a0f236178f729ddbc92ae91e903dbc31925a6
kulaxams <mario.jalsovec@mindsmiths.com>
2023-01-12 18:00:18 +0100

    local setup

---
+++ local-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Install gitleaks
+
+cd /tmp
+git clone https://github.com/zricethezav/gitleaks.git
+cd gitleaks
+make build
+cp gitleaks /usr/bin
+
+git config --global init.templatedir '~/.git-templates'
+
+mkdir -p ~/.git-templates/hooks
+
+cp pre-commit ~/.git-templates/hooks
+
+cp gitleaks.toml ~/.git-templates
